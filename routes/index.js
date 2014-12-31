a8ca204269479b04a426328f2e0d89b7addfb487
wei2 <b01705015@ntu.edu.tw>
2014-12-31 17:05:00 +0800

    notifications clear

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,30 @@
 /*
  * GET home page.
  */
+var Notification = require('../models').Notification;
+var _ = require('underscore');
 
 exports.index = function(req, res){
 	var config = {};
 	// console.log(req.session)
 	config.isLogin = req.session.isLogin ? true : false;
-		if(config.isLogin)
-			config.User = req.session.user;
+		if(config.isLogin){
+			Notification.findAll({
+				where: {
+					member_id: req.session.user.member_id,
+					read: "unread"
+				}
+			}).success(function( notifications ){
+				config.Notifications =  _.size(notifications);
+				// console.log("notifications: ="+config.Notifications);
+				config.User = req.session.user;
+				res.render('index', config);
+			});
+			
+		}
 		else
 			config.User = false;
-  	res.render('index', config);
+  	
 };
 
 exports.partial = function (req, res) {
